2527cfa657b627646038a5c2bb5aab34f1e23999
panlin520 <40063365+panlin520@users.noreply.github.com>
2026-09-14 00:19:39 +0800

    Add progress output to DBNReader test

--- tests/marketdata/DBNReaderTest.cpp
+++ tests/marketdata/DBNReaderTest.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <iostream>
+
 #include "trading/marketdata/databento/DBNReader.hpp"
 
 using namespace CMETradingSystem::MarketData::Databento;
@@ -7,11 +9,27 @@ using namespace CMETradingSystem::MarketData::Databento;
 
 TEST(DBNReaderTest, OpenClose)
 {
+    std::cout << "[DBNReaderTest] START" << std::endl;
+
+    std::cout << "[1/4] Creating DBNReader" << std::endl;
+
     DBNReader reader(
         "00_data/ESU6_2026-06-15_MBO.dbn.zst"
     );
 
-    EXPECT_TRUE(reader.open());
+    std::cout << "[2/4] Opening DBN file" << std::endl;
+
+    bool opened = reader.open();
+
+    std::cout << "[3/4] Open result: "
+              << (opened ? "SUCCESS" : "FAILED")
+              << std::endl;
+
+    EXPECT_TRUE(opened);
+
+    std::cout << "[4/4] Checking reader state" << std::endl;
 
     EXPECT_TRUE(reader.is_open());
+
+    std::cout << "[DBNReaderTest] FINISHED" << std::endl;
 }
