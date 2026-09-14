52c76c9be837326df5e9bf2f546ec2e46a8c1008
panlin520 <40063365+panlin520@users.noreply.github.com>
2026-09-14 15:43:16 +0800

    Add DBNRecord implementation file

---
+++ src/trading/marketdata/databento/DBNRecord.cpp
@@ -0,0 +1,9 @@
+#include "trading/marketdata/databento/DBNRecord.hpp"
+
+namespace CMETradingSystem::MarketData::Databento
+{
+
+// DBNRecord is currently a data container.
+// Parsing logic will be implemented in DBNReader::next_record().
+
+}
