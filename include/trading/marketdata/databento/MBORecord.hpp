39afbca843d66c61b76d2fdaf45733d12098450d
panlin520 <40063365+panlin520@users.noreply.github.com>
2026-09-14 16:08:16 +0800

    Add Databento MBORecord structure

---
+++ include/trading/marketdata/databento/MBORecord.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace CMETradingSystem::MarketData::Databento
+{
+
+// Databento Market By Order intermediate structure.
+// Binary DBN data will be decoded into this structure.
+
+struct MBORecord
+{
+    uint64_t ts_event{0};
+    uint64_t ts_recv{0};
+
+    uint8_t rtype{0};
+
+    uint16_t publisher_id{0};
+    uint32_t instrument_id{0};
+
+    char action{0};
+    char side{0};
+
+    int64_t price{0};
+    uint32_t size{0};
+
+    uint16_t channel_id{0};
+    uint64_t order_id{0};
+
+    uint8_t flags{0};
+    int32_t ts_in_delta{0};
+
+    uint64_t sequence{0};
+
+    std::string symbol;
+};
+
+}
